c5f087e210fe6eed3eb69e5813a9a25082bf58e2
Dau Doan Hoan Thien <Hoan64735@gmail.com>
2025-02-17 21:56:40 +0700

    commit lan dau

---
+++ src/java/control/HomeControl.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import dao.DAO;
+import entity.Category;
+import entity.Product;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+
+/**
+ *
+ * @author trinh
+ */
+@WebServlet(name = "HomeControl", urlPatterns = {"/home"})
+public class HomeControl extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        //b1: get data from dao
+        DAO dao = new DAO();
+        List<Product> list = dao.getAllProduct();
+        System.out.println("Số lượng sản phẩm lấy được: " + list.size()); // Debug
+        request.setAttribute("listP", list);
+        List<Category> listC = dao.getAllCategory();
+        Product last = dao.getLast();
+        
+        //b2: set data to jsp
+        request.setAttribute("listCC", listC);
+        request.setAttribute("p", last);
+        request.getRequestDispatcher("Home.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
