6011bc691524fdb767d1d9a90b7cf1f56c5ee16e
Dau Doan Hoan Thien <Hoan64735@gmail.com>
2025-02-18 23:45:23 +0700

    commit lan 2

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -1,8 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dao;
 
 import context.DBContext;
 import entity.Category;
-//import entity.Category;
 import entity.Product;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,39 +25,100 @@ public class DAO {
     ResultSet rs = null;
 
     public List<Product> getAllProduct() {
-    List<Product> list = new ArrayList<>();
-    String query = "SELECT * FROM product";
-    try {
-        conn = new DBContext().getConnection();
-        ps = conn.prepareStatement(query);
-        rs = ps.executeQuery();
-        while (rs.next()) {
-            Product p = new Product(rs.getInt(1),
-                                    rs.getString(2),
-                                    rs.getString(3),
-                                    rs.getDouble(4),
-                                    rs.getString(5),
-                                    rs.getString(6));
-            list.add(p);
-        }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-    return list;
-}
-
-
-
-    public List<Category> getAllCategory() {
-       List<Category> list = new ArrayList<>();
-      String query = "select * from Category";
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product";
         try {
             conn = new DBContext().getConnection();//mo ket noi voi sql
             ps = conn.prepareStatement(query);
             rs = ps.executeQuery();
-           while (rs.next()) {
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> getProductByCID(String cid) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where cateID = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, cid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public List<Product> searchByName(String txtSearch) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where [name] like ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1,"%"+ txtSearch+"%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public Product getProductByID(String id) {
+        String query = "select * from product\n"
+                + "where id = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public List<Category> getAllCategory() {
+        List<Category> list = new ArrayList<>();
+        String query = "select * from Category";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
                 list.add(new Category(rs.getInt(1),
-                       rs.getString(2)));
+                        rs.getString(2)));
             }
         } catch (Exception e) {
         }
@@ -67,7 +132,7 @@ public class DAO {
             conn = new DBContext().getConnection();//mo ket noi voi sql
             ps = conn.prepareStatement(query);
             rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 return new Product(rs.getInt(1),
                         rs.getString(2),
                         rs.getString(3),
@@ -89,7 +154,5 @@ public class DAO {
             System.out.println(o);
         }
     }
-    
-    
 
 }
